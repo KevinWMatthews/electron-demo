23800dfc6aea41e7ebdee6bbff8f6fdb17d22d65
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-09 09:42:10 -0700

    Load local html into a window

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('running main.js')
+// node.js API: https://nodejs.org/api/
+// Electron API: https://electronjs.org/docs/api
+
+// "Import" the app and BrowserWindow objects from the electron library.
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+}
+
+app.on('ready', createWindow);
